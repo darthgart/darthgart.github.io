a84441201877c7673102eea8fbe846e14bddc20c
Edgar Sanchez <113390851+darthgart@users.noreply.github.com>
2023-11-15 12:15:47 +0100

    Update hero.tsx

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -15,8 +15,8 @@ export default function Hero() {
     return (
             <section id='hero' className=''>
                 <div className='grid grid-cols-1 sm:grid-cols-2'>
-                <div className='relative col-span-1 h-full min-h-[200px] w-full overflow-hidden shadow-xl shadow-black/40  sm:rounded-r-3xl'>
-                    <img className='object-cover' alt='Edgar-Image' decoding='async' sizes='full' src='img1.jpg' />
+                <div className='relative col-span-1 h-full min-h-[200px] w-full sm:min-w-[300px] overflow-hidden shadow-xl shadow-black/40 sm:rounded-r-3xl'>
+                    <img className='object-cover w-full h-full' alt='Edgar-Image' decoding='async' sizes='full' src='img1.jpg' />
                 </div>
                 <div className='relative col-span-1 sm:-ml-4 sm:mr-4 sm:py-6 md:-ml-8 md:mr-8 md:py-12'>
                     <div className='z-20 flex w-full flex-col items-start overflow-visible bg-stone-800 px-4 py-8 shadow-xl shadow-black/30 sm:rounded-2xl sm:p-8 lg:p-12'>
